0193e2fe517a1e5b0b4f38cbf0fcc7aa3b16bc07
brf153 <153hsb@gmail.com>
2024-03-15 17:33:44 +0530

    replace mui select with html select
    Signed-off-by: brf153 <153hsb@gmail.com>

--- app/component/Navbar.tsx
+++ app/component/Navbar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { ChangeEvent } from "react";
 import { CiSearch } from "react-icons/ci";
 import { CiBellOn } from "react-icons/ci";
 import { IoSettingsOutline } from "react-icons/io5";
@@ -14,10 +14,10 @@ import pic from "@/public/pic.png";
 type Props = {};
 
 const Navbar = (props: Props) => {
-  const [age, setAge] = React.useState("");
+  const [value, setValue] = React.useState("automobiles");
 
   const handleChange = (event: SelectChangeEvent) => {
-    setAge(event.target.value as string);
+    setValue(event.target.value as string);
   };
 
   return (
@@ -33,31 +33,22 @@ const Navbar = (props: Props) => {
             placeholder="Search"
             className="bg-[#292F42] focus:outline-none p-2 placeholder:text-[#F1F5F9] rounded-lg"
           />
-          <CiSearch className=" w-6 h-6 text-gray-400 pointer-events-none mt-2 mr-2" />
+          <CiSearch className=" w-6 h-6 text-gray-400 pointer-events-none mt-3 mr-2" />
         </div>
       </div>
       <div className="flex gap-4 w-fit items-center mr-6">
         <CiBellOn className="text-xl w-[2vmax] h-[2vmax]" />
         <IoSettingsOutline className="text-xl w-[2vmax] h-[2vmax]" />
 
-        <FormControl className="w-[10vmax] border-white">
-          <InputLabel
-            id="demo-simple-select-label"
-            className="text-white w-fit border-white"
+          <select
+            value={value}
+            className="text-white border-white bg-[#292F42] p-2"
+            onChange={(event: any) => handleChange(event)}
           >
-            Automobiles
-          </InputLabel>
-          <Select
-            value={age}
-            label="Age"
-            onChange={handleChange}
-            className="text-white border-white"
-          >
-            <MenuItem value={10}>Tesla</MenuItem>
-            <MenuItem value={20}>Mercedes</MenuItem>
-            <MenuItem value={30}>BMW</MenuItem>
-          </Select>
-        </FormControl>
+            <option value="tesla">Tesla</option>
+            <option value="mercedes">Mercedes</option>
+            <option value="bmw">BMW</option>
+          </select>
 
         <Image alt="Remy Sharp" className="w-10" src={pic} />
       </div>
